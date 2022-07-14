d97417ce9c364cb21f7994581bb014703a7266c3
Pavel Lovkii <plovkiy@yandex.ru>
2022-07-15 01:47:51 +0700

    feat: save expenses to a file

--- js/script.js
+++ js/script.js
@@ -1,11 +1,73 @@
-$(document).ready(function(){
-    $('button.btn-expenses__add').click(function () {
-        console.log(this);
-        this.textContent = (this.textContent === 'Cancel') ? 'Add New' : 'Cancel';
-        $('form.expenses__add').toggleClass('hidden');
-    });
+let fileHandle;
+let expenses = [];
 
-    $('form.expenses__add').submit(function (e) { 
-        e.preventDefault();
-    });
+$(document).ready(function () {
+  $(".btn-expenses__upload").click(async function () {
+    [fileHandle] = await window.showOpenFilePicker();
+    let fileData = await fileHandle.getFile();
+    let rawExpenses = await fileData.text();
+    expenses = JSON.parse(rawExpenses);
+    window.localStorage.setItem("expenses", rawExpenses);
+    renderExpenses();
+  });
+
+  $(".btn-expenses__save").click(async function () {
+    saveDB(JSON.stringify(expenses));
+  });
+
+  $("button.btn-expenses__add").click(function () {
+    this.textContent = this.textContent === "Cancel" ? "Add New" : "Cancel";
+    $("form.expenses__add").toggleClass("hidden");
+  });
+
+  $("form.expenses__add").submit(function (e) {
+    e.preventDefault();
+    const expense = {
+      name: this.querySelector("#expense-name").value,
+      date: this.querySelector("#expense-date").value,
+      category: this.querySelector("#expense-category").value,
+      amount: this.querySelector("#expense-amount").value,
+      comment: this.querySelector("#expense-category").value,
+    };
+    expenses.push(expense);
+    window.localStorage.setItem("expenses", JSON.stringify(expenses));
+  });
 });
+
+const saveDB = async function (data) {
+  fileHandle = await window.showSaveFilePicker();
+  let stream = await fileHandle.createWritable();
+  await stream.write(data);
+  await stream.close();
+};
+
+const renderExpenses = function () {
+  const data = JSON.parse(window.localStorage.getItem("expenses"));
+  expenses = data;
+  data.forEach((element) => {
+    renderSingleExpense(element);
+  });
+};
+
+const renderSingleExpense = function (expense) {
+  const table = document.querySelector(".expenses__table");
+  const markup = `
+    <tr class="expenses__row">
+        <td class="expenses__column">
+            ${expense.date}
+        </td>
+        <td class="expenses__column">
+            ${expense.name}
+        </td>
+        <td class="expenses__column">
+            ${expense.category}
+        </td>
+        <td class="expenses__column">
+            ${expense.amount}
+        </td>
+        <td class="expenses__column">
+            ${expense.comment}
+        </td>
+    </tr>`;
+  table.insertAdjacentHTML("beforeend", markup);
+};
