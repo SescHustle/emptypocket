bd07bcc430eba14a8bec007f81ba223e4cc92b4e
Pavel Lovkii <plovkiy@yandex.ru>
2022-10-29 15:33:05 +0700

    feat: add spendings api routes

--- server/server.js
+++ server/server.js
@@ -9,5 +9,27 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.status(200).send("Hello World!");
 });
+
+app.get("/api/spendings/", (req, res) => {
+  res.status(200).send("Get all spendings");
+});
+
+app.get("/api/spendings/:id", (req, res) => {
+  res.status(200).send(`Get spending with id ${req.params.id}`);
+});
+
+app.post("/api/spendings/", (req, res) => {
+  res.status(200).send("Add new spending");
+});
+
+app.patch("/api/spendings/:id"),
+  (req, res) => {
+    res.status(200).send(`Update spending with id ${req.params.id}`);
+  };
+
+app.delete("/api/spendings/:id"),
+  (req, res) => {
+    res.status(200).send(`Delete spending with id ${req.params.id}`);
+  };
