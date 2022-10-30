acd1b9357109ea3d08308c9ba70ee09410ed2914
Pavel Lovkii <plovkiy@yandex.ru>
2022-10-30 17:13:42 +0700

    feat: init App.js

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1>Hello React!</h1>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./component/Header";
-import Footer from "./component/Footer";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <React.StrictMode>
-    <Header />
-    <h1>Hello React!</h1>
-    <Footer />
-  </React.StrictMode>
-);
+root.render(<App />);
