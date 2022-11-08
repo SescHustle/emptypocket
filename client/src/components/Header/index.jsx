4ef5c3df0d5447b475358d771892a46de89d0a26
Pavel Lovkii <plovkiy@yandex.ru>
2022-11-09 00:41:31 +0700

    feat: F-2 - add bootstrap and fix header styles

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,39 +1,52 @@
 import React from 'react';
-import logo from '../../assets/logo.png'
+import logo from '../../assets/logo.png';
 
-function Header () {
-  return (
-    <header className="header" id="header">
-    <nav className="navbar navbar-expand-md navbar-dark bg-primary header-nav">
-        <div className="container-fluid header__row">
-            <a className="header-nav__logo" href="#">
-                <img src={logo} alt="App Logo" />
-            </a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav header-nav__links">
-                    <li className="nav-item header-nav__link">
-                        <a href="index.html">Overview</a>
-                    </li>
-                    <li className="nav-item header-nav__link ml-0">
-                        <a href="expenses.html">Expenses</a>
-                    </li>
-                    <li className="nav-item header-nav__link">
-                        <a href="#">Reports</a>
-                    </li>
-                    <li className="nav-item header-nav__link">
-                        <a href="#">Planning</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-</header>
-  );
-};
+function Header() {
+	return (
+		<header id='header'>
+			<nav className='navbar navbar-expand-md bg-info'>
+				<div className='container-fluid'>
+					<a className='navbar-brand' href='/'>
+						<img src={logo} className='img-fluid' width='32px' alt='App Logo' />
+					</a>
+					<button
+						className='navbar-toggler'
+						type='button'
+						data-bs-toggle='collapse'
+						data-bs-target='#navbarSupportedContent'
+						aria-controls='navbarSupportedContent'
+						aria-expanded='false'
+						aria-label='Toggle navigation'>
+						<span className='navbar-toggler-icon' />
+					</button>
+					<div className='collapse navbar-collapse' id='navbarSupportedContent'>
+						<ul className='navbar-nav'>
+							<li className='nav-item'>
+								<a className='nav-link' href='/'>
+									Overview
+								</a>
+							</li>
+							<li className='nav-item'>
+								<a className='nav-link' href='/'>
+									Expenses
+								</a>
+							</li>
+							<li className='nav-item'>
+								<a className='nav-link' href='/'>
+									Reports
+								</a>
+							</li>
+							<li className='nav-item'>
+								<a className='nav-link' href='/'>
+									Planning
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</header>
+	);
+}
 
 export default Header;
