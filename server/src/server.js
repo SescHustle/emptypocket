d995629855397725a934244faa9ccf81509fbb29
Pavel Lovkii <plovkiy@yandex.ru>
2023-03-17 22:18:44 +0700

    fix: B-1 - remove routes from servers.js

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,7 @@ const config = require('../config/config.json');
 
 const app = express();
 const {port} = config;
+const spendingsRoutes = require('./Router/Spendings');
 
 app.listen(port, () => {
 	console.log(`Express app is working at http://localhost:${port}`);
@@ -12,22 +13,4 @@ app.get('/', (req, res) => {
 	res.status(200).send('Hello World!');
 });
 
-app.get('/api/spendings/', (req, res) => {
-	res.status(200).send('Get all spendings');
-});
-
-app.get('/api/spendings/:id', (req, res) => {
-	res.status(200).send(`Get spending with id ${req.params.id}`);
-});
-
-app.post('/api/spendings/', (req, res) => {
-	res.status(200).send('Add new spending');
-});
-
-app.patch('/api/spendings/:id', (req, res) => {
-	res.status(200).send(`Update spending with id ${req.params.id}`);
-});
-
-app.delete('/api/spendings/:id', (req, res) => {
-	res.status(200).send(`Delete spending with id ${req.params.id}`);
-});
+app.use('/api/spendings', spendingsRoutes);
