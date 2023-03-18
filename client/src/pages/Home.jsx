00666368777f03fee44696d78a42506f8b05dda1
Pavel Lovkii <plovkiy@yandex.ru>
2023-03-18 21:33:09 +0700

    chore: minor frontend fixes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,19 @@
+import React from 'react';
+
 function Home() {
-    return (
-        <main class="main">
-            <div class="main__header">
-                <h1 class="h1">Overview</h1>
-            </div>
-            <div class="main__body">
-                <h2 class="h2">This page is in development.</h2>
-                <p>You can visit <a href="expenses.html">expenses</a> page.</p>
-            </div>
-        </main>
-    )
+	return (
+		<main className='main'>
+			<div className='main__header'>
+				<h1 className='h1'>Overview</h1>
+			</div>
+			<div className='main__body'>
+				<h2 className='h2'>This page is in development.</h2>
+				<p>
+					You can visit <a href='expenses.html'>expenses</a> page.
+				</p>
+			</div>
+		</main>
+	);
 }
 
 export default Home;
